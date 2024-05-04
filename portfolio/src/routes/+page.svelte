e473442a99d914619da6fe31f4d09651bf544f87
Jobet Casquejo <87087925+jobet1995@users.noreply.github.com>
2024-05-04 03:12:41 +0000

    added

---
+++ portfolio/src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<style>
+  @import './assets/css/styles.css';
+</style>
+<header id="header" class="fixed-top">
+  <div class="container-fluid d-flex justify-content-between align-items-center">
+    <nav id="navbar" class="navbar order-last order-lg-0">
+      <ul>
+        <li><a class="active" href="index.html">Home</a></li>
+        <li><a href="about.html">About</a></li>
+        <li><a href="resume.html">Resume</a></li>
+        <li><a href="services.html">Services</a></li>
+        <li><a href="portfolio.html">Portfolio</a></li>
+        <li><a href="contact.html">Contact</a></li>
+      </ul>
+      <i class="bi bi-list mobile-nav-toggle"></i>
+    </nav>
+  </div>
+</header>
